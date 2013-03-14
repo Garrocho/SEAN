352b2cab220810634b47095bc6d7d9f62b5acc54
CharlesGarrocho <charles.garrocho@gmail.com>
2013-03-14 11:54:19 -0300

    adicionado: classe responsável por tratar as conexões com o SEMON.

---
+++ source/src/com/sean/net/Conexao.java
@@ -0,0 +1,95 @@
+package com.sean.net;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import org.itadaki.bzip2.BZip2OutputStream;
+
+public class Conexao {
+
+	private OutputStream enviaDados;
+	private InputStream recebeDados;
+	
+	private Socket soquete;
+	private int PORTA;
+	private String ENDERECO;
+
+	public Conexao() {
+		super();
+		this.ENDERECO = "192.168.10.101";
+		this.PORTA = 7777;
+	}
+
+	public Conexao(String endereco, int porta) {
+		super();
+		this.ENDERECO = endereco.substring(endereco.indexOf("/") + 1 );
+		this.PORTA = porta;
+	}
+
+	public boolean conectaServidor() {
+
+		try {
+			InetAddress endereco = InetAddress.getByName(ENDERECO);
+			soquete = new Socket(endereco, PORTA);
+			enviaDados = soquete.getOutputStream();
+			recebeDados = soquete.getInputStream();
+			return true;
+		}catch (Exception e) {
+			return false;
+		}
+	}
+
+	public void desconectaServidor() throws IOException {
+
+		recebeDados.close();
+		enviaDados.close();
+		soquete.close();
+	}
+	
+	public String byteParaString(InputStream in) throws IOException {
+        StringBuffer out = new StringBuffer();
+        byte[] b = new byte[4096];
+        for (int i; (i = in.read(b)) != -1;) {
+            out.append(new String(b, 0, i));
+        }
+        return out.toString();
+    }
+	
+	public static byte[] bz2Compress(String mensagem) {
+		try {
+			ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+			BZip2OutputStream output = new BZip2OutputStream (byteOutput);
+			output.write(mensagem.getBytes());
+			output.close();
+			return byteOutput.toByteArray();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public OutputStream getEnviaDados() {
+		return enviaDados;
+	}
+
+	public InputStream getRecebeDados() {
+		return recebeDados;
+	}
+
+	public Socket getSoquete() {
+		return soquete;
+	}
+
+	public int getPORTA() {
+		return PORTA;
+	}
+
+	public String getENDERECO() {
+		return ENDERECO;
+	}
+
+}
