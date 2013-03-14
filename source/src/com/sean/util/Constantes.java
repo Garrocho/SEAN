4f2e7966b4deaba5447a817a02cb2bea89881993
CharlesGarrocho <charles.garrocho@gmail.com>
2013-03-14 11:56:12 -0300

    adicionado: classe responsável por guardar as strings padrões de troca de mensagens com o servidor.

---
+++ source/src/com/sean/util/Constantes.java
@@ -0,0 +1,18 @@
+package com.sean.util;
+
+public class Constantes {
+	
+	// codigos de requisicao
+	public static String STATUS = "STATUS";
+	public static String LOGAR = "LOGAR_";
+	public static String INICIAR = "INICIA";
+	public static String PAUSAR = "PAUSAR";
+	public static String IMAGEM = "IMAGEM";
+	public static String DESLIGAR = "DESLIG";
+	public static String CONFIGURAR = "CONFIG";
+	
+	// codigos de resposta
+	public static String OK_200 = "OK_200";
+	public static String NAO_AUTORIZADO_401 = "NO_401";
+
+}
